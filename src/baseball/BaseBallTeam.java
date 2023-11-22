fc4bc1daccaaaf0da3d6f85cb5bb0b1fb65442c5
sakinakamura19 <saki.nakamura.19@outlook.jp>
2023-11-23 00:47:15 +0900

    プロ野球チームの勝ち負け数を管理しよう

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,62 @@
+package baseball;
+
+public class BaseBallTeam {
+
+	private String name;
+	private int win;
+	private int lose;
+	private int draw;
+
+	public BaseBallTeam() {
+	}
+
+	public BaseBallTeam(String name, int win, int lose, int draw) {
+		this.setName(name);
+		this.setWin(win);
+		this.setLose(lose);
+		this.setDraw(draw);
+	}
+
+	public void report() {
+		double result = getRate();
+		System.out.println(getName() + " の2022年の成績は " + getWin() + "勝 " + getLose() + "敗 " + getDraw() + "分、勝率は　"
+				+ result + "です。");
+	}
+
+	public double getRate() {
+		double result = (double) getWin() / (getWin() + getLose());
+		return result;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWin() {
+		return win;
+	}
+
+	public void setWin(int win) {
+		this.win = win;
+	}
+
+	public int getLose() {
+		return lose;
+	}
+
+	public void setLose(int lose) {
+		this.lose = lose;
+	}
+
+	public int getDraw() {
+		return draw;
+	}
+
+	public void setDraw(int draw) {
+		this.draw = draw;
+	}
+}
